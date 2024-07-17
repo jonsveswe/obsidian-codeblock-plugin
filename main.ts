31e03f4618f6dd717e81e894eaef4ab027fbd75d
Jonas Svensson <jonas.svensson@midsummer.se>
2024-07-17 17:08:49 +0200

    feat: add support for running CSharp code

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import { Plugin, MarkdownPostProcessorContext, MarkdownView, Notice, PluginSettingTab, App, Setting } from 'obsidian';
 import * as child_process from 'child_process';
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
 
 export const runButtonClass = "run-code-button";
 const hasButtonClass = "has-run-code-button";
@@ -18,7 +21,7 @@ const DEFAULT_SETTINGS: ExecutePythonSettings = {
 
 export default class ExecutePython extends Plugin {
     settings: ExecutePythonSettings;
-
+    tempFileId: string | undefined = undefined;
     async onload() {
         await this.loadSettings();
 
@@ -28,7 +31,7 @@ export default class ExecutePython extends Plugin {
 
         this.registerMarkdownPostProcessor((element, _context) => {
 			this.addRunButtons(element, _context.sourcePath);
-		});
+		}); 
     }
 
     /**
@@ -78,12 +81,18 @@ export default class ExecutePython extends Plugin {
 			button.addEventListener("click", async () => {
                 await this.runJavascriptCode(code);
             });
-        } else {
-
+        } else if(language === "language-python") {
+            button.addEventListener("click", async () => {
+                await this.runPythonCode2(code);
+            });
+        } else if(language === "language-csharp") {
+            button.addEventListener("click", async () => {
+                await this.runCSharpCode(code);
+            });
         }
     }
     private async runJavascriptCode(code: string){
-        console.log("Inside runCode");
+        console.log("Inside runJavascriptCode");
         console.log("code: " + code);
         try {
             new Function(code)();
@@ -91,6 +100,86 @@ export default class ExecutePython extends Plugin {
             console.error(err);
         }
     }
+
+    private async runPythonCode2(code: string){
+        console.log("Inside runPythonCode");
+        console.log("code: " + code);
+        let pythonProcess: child_process.ChildProcessWithoutNullStreams;
+        let handleOutput: Promise<void>;
+        let pythonPath = "python"; // Needs to have Python installed globally and added to PATH. For example, C:\Users\jonas.svensson\AppData\Local\Programs\Python\Python39\python.exe
+        pythonProcess = child_process.spawn(pythonPath, ['-u', '-c', code]);
+
+        handleOutput = new Promise((resolve, reject) => {
+            pythonProcess.stdout.on('data', (data) => {
+                console.log(`stdout: ${data}`);
+            });
+            pythonProcess.stderr.on('data', (data) => {
+                console.error(`stderr: ${data}`);
+            });
+            pythonProcess.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+                resolve();
+            });
+        });
+        try {
+            await handleOutput;
+          } catch (err) {
+            console.error(err);
+        }
+    }
+    private async runCSharpCode(code: string){
+        console.log("Inside runCSharpCode");
+        console.log("code: " + code);
+        let csharpProcess: child_process.ChildProcessWithoutNullStreams;
+        let handleOutput: Promise<void>;
+        let csharpPath = "C:/Users/jonas.svensson/.dotnet/tools/dotnet-script.exe"; // install dotnet core sdk https://dotnet.microsoft.com/en-us/download and run in command line "dotnet tool install -g dotnet-script"
+        //let cmd = `C:/Users/jonas.svensson/.dotnet/tools/dotnet-script.exe`;
+        const tempFileName = this.getTempFile("csx"); // C:\Users\JONAS~1.SVE\AppData\Local\Temp\temp_1721227658357.csx
+        //let tempFileName = "C:/tempfile.csx";
+        let args = [tempFileName];
+        fs.writeFileSync(tempFileName, code);
+        console.log("csharpPath: ", csharpPath);
+        console.log("args: ", args);
+        console.log("env: ", process.env);
+        console.log("tempFileName: ", tempFileName);
+        // Spawn a new process to run the C# code using the dotnet-script CLI tool.
+        // The `csharpPath` is the path to the dotnet-script CLI tool that should be used to run the code.
+        // args[0] have the file path to the C# code.
+        csharpProcess = child_process.spawn(csharpPath, args, { env: process.env, shell: true });
+        handleOutput = new Promise((resolve, reject) => {
+            csharpProcess.stdout.on('data', (data) => {
+                console.log(`stdout: ${data}`);
+            });
+            csharpProcess.stderr.on('data', (data) => {
+                console.error(`stderr: ${data}`);
+            });
+            csharpProcess.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+                this.tempFileId = undefined; // Reset the file id to use a new file next time
+                resolve();
+            });
+        });
+        try {
+            await handleOutput;
+          } catch (err) {
+            console.error(err);
+        }
+    }
+	/**
+	 * Creates a new unique file name for the given file extension. The file path is set to the temp path of the os.
+	 * The file name is the current timestamp: '/{temp_dir}/temp_{timestamp}.{file_extension}'
+	 * this.tempFileId will be updated, accessible to other methods
+	 * Once finished using this value, remember to set it to undefined to generate a new file
+	 *
+	 * @param ext The file extension. Should correspond to the language of the code.
+	 * @returns The temporary file path
+	 */
+	private getTempFile(ext: string) {
+		if (this.tempFileId === undefined)
+			this.tempFileId = Date.now().toString();
+		console.log("temp file: ", path.join(os.tmpdir(), `temp_${this.tempFileId}.${ext}`));
+		return path.join(os.tmpdir(), `temp_${this.tempFileId}.${ext}`);
+	}
 
     processPythonCodeBlock(source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
         let noInput = source.includes("#noinput");
@@ -152,7 +241,11 @@ export default class ExecutePython extends Plugin {
         };
 
         const run = async () => {
+            // Spawn a new process to run the Python code. The '-u' flag specifies that the output should be unbuffered, and '-c' specifies that the following argument should be treated as a command to be executed.
+            // The `this.settings.pythonPath` is the path to the Python executable that should be used to run the code.
+            // The `source` argument is the code to be executed.
             pythonProcess = child_process.spawn(this.settings.pythonPath, ['-u', '-c', source]);
+            console.log("this.settings.pythonPath: ", this.settings.pythonPath)// python
 
             const submitInput = () => {
                 pythonProcess.stdin.write(inputField.value + "\n");
