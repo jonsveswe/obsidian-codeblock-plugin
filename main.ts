0ba010a39e86ce6bc89fb2f58c79340a1004a4be
Jonas Svensson <jonas.svensson@midsummer.se>
2024-07-18 16:36:17 +0200

    refactor: remove unused code

--- main.ts
+++ main.ts
@@ -7,27 +7,9 @@ import * as path from "path";
 export const runButtonClass = "run-code-button";
 const hasButtonClass = "has-run-code-button";
 
-interface ExecutePythonSettings {
-    pythonPath: string;
-    showCodeInPreview: boolean;
-    showExitCode: boolean;
-}
-
-const DEFAULT_SETTINGS: ExecutePythonSettings = {
-    pythonPath: 'python',
-    showCodeInPreview: true,
-    showExitCode: false
-}
-
-export default class ExecutePython extends Plugin {
-    settings: ExecutePythonSettings;
+export default class RunCodeblockPlugin extends Plugin {
     tempFileId: string | undefined = undefined;
     async onload() {
-        await this.loadSettings();
-
-        this.addSettingTab(new MyPluginSettingTab(this.app, this));
-
-        //this.registerMarkdownCodeBlockProcessor('python', this.processPythonCodeBlock.bind(this));
 
         this.registerMarkdownPostProcessor((element, _context) => {
 			this.addRunButtons(element, _context.sourcePath);
@@ -83,7 +65,7 @@ export default class ExecutePython extends Plugin {
             });
         } else if(language === "language-python") {
             button.addEventListener("click", async () => {
-                await this.runPythonCode2(code);
+                await this.runPythonCode(code);
             });
         } else if(language === "language-csharp") {
             button.addEventListener("click", async () => {
@@ -105,7 +87,7 @@ export default class ExecutePython extends Plugin {
         }
     }
 
-    private async runPythonCode2(code: string){
+    private async runPythonCode(code: string){
         console.log("Inside runPythonCode");
         console.log("code: " + code);
         let pythonProcess: child_process.ChildProcessWithoutNullStreams;
@@ -224,180 +206,4 @@ export default class ExecutePython extends Plugin {
 		console.log("temp file: ", path.join(os.tmpdir(), `temp_${this.tempFileId}.${ext}`));
 		return path.join(os.tmpdir(), `temp_${this.tempFileId}.${ext}`);
 	}
-
-    processPythonCodeBlock(source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
-        let noInput = source.includes("#noinput");
-	    source = source.replace("#noinput\n", "");
-	    source = source.replace("#noinput", "");
-
-        if (this.settings.showCodeInPreview) {
-            let codeBlock = el.createEl('pre');
-            let codeElement = codeBlock.createEl('code');
-            codeElement.className = "language-python";
-            codeElement.textContent = source;
-        }
-
-        const outputArea = el.createEl('div', { cls: 'python-output', attr: {style: 'white-space: pre-wrap;'} });
-
-        this.runPythonCode(source, outputArea, noInput);
-    }
-
-/*     addRunButtons(mdView: MarkdownView, el: HTMLElement) {
-        el.querySelectorAll('pre.language-python').forEach((block: HTMLPreElement) => {
-            let noInput = block.textContent.includes("#noinput");
-            if (this.settings.showCodeInPreview) {
-                let codeBlock = block.createEl('pre');
-                let codeElement = codeBlock.createEl('code');
-                codeElement.className = "language-python";
-                codeElement.textContent = block.textContent;
-            }
-
-            const source = block.querySelector('code')?.textContent || '';
-            const outputArea = block.createEl('div', { cls: 'python-output', attr: {style: 'white-space: pre-wrap;'} });
-
-            this.runPythonCode(source, outputArea, noInput);
-
-        });
-    } */
-
-    async runPythonCode(source: string, outputArea: HTMLElement, noInput: boolean = false) {
-        console.log("Inside runPythonCode");
-        outputArea.textContent = '';  // Clear the output area before each run
-
-        let inputField;
-        let submitButton;
-        if (!noInput) {
-            inputField = outputArea.createEl('input', { attr: {type: 'text'} });
-            submitButton = outputArea.createEl('button', { text: 'Submit Input' });
-        }
-        let runButton = outputArea.createEl('button', { text: 'Start' });
-        let resetButton = outputArea.createEl('button', { text: 'Reset' });
-        let outputPre = null;
-        
-        let pythonProcess: child_process.ChildProcessWithoutNullStreams;
-        let handleOutput: Promise<void>;
-
-        const reset = () => {
-            pythonProcess && pythonProcess.kill();
-            inputField && (inputField.value = '');
-            outputPre && outputArea.removeChild(outputPre);
-            outputPre = null;
-        };
-
-        const run = async () => {
-            // Spawn a new process to run the Python code. The '-u' flag specifies that the output should be unbuffered, and '-c' specifies that the following argument should be treated as a command to be executed.
-            // The `this.settings.pythonPath` is the path to the Python executable that should be used to run the code.
-            // The `source` argument is the code to be executed.
-            pythonProcess = child_process.spawn(this.settings.pythonPath, ['-u', '-c', source]);
-            console.log("this.settings.pythonPath: ", this.settings.pythonPath)// python
-
-            const submitInput = () => {
-                pythonProcess.stdin.write(inputField.value + "\n");
-                inputField.value = '';
-            };
-
-            if (!noInput) {
-                submitButton.addEventListener('click', submitInput);
-                inputField.addEventListener('keyup', (event) => {
-                    if (event.key === 'Enter') {
-                        submitInput();
-                    }
-                });
-            }
-
-            handleOutput = new Promise((resolve, reject) => {
-                pythonProcess.stdout.on('data', (data) => {
-                    if (!outputPre) {
-                        outputPre = outputArea.createEl('pre');
-                    }
-                    outputPre.append(data.toString());
-                });
-
-                pythonProcess.stderr.on('data', (data) => {
-                    if (!outputPre) {
-                        outputPre = outputArea.createEl('pre');
-                    }
-                    outputPre.append(`Error: ${data}`);
-                });
-
-                pythonProcess.on('close', (code) => {
-                    if (this.settings.showExitCode) {
-                        if (!outputPre) {
-                            outputPre = outputArea.createEl('pre');
-                        }
-                        outputPre.append(`\nPython exited with code: ${code}`);
-                    }
-                    resolve();
-                });
-
-                pythonProcess.on('error', (err) => {
-                    reject(err);
-                });
-            });
-
-            try {
-                await handleOutput;
-            } catch (err) {
-                if (!outputPre) {
-                    outputPre = outputArea.createEl('pre');
-                }
-                outputPre.append(`\nAn error occurred: ${err}`);
-            }
-        };
-
-        runButton.addEventListener('click', run);
-        resetButton.addEventListener('click', reset);
-    }
-
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
-}
-
-class MyPluginSettingTab extends PluginSettingTab {
-    plugin: ExecutePython;
-
-    constructor(app: App, plugin: ExecutePython) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
-
-    display(): void {
-        let {containerEl} = this;
-
-        containerEl.empty();
-        new Setting(containerEl)
-            .setName('Python')
-            .setDesc('The command used to invoke Python on your system. (ex. python or python3)')
-            .addText(text => text
-                .setValue(this.plugin.settings.pythonPath)
-                .onChange(async (value) => {
-                    this.plugin.settings.pythonPath = value;
-                    await this.plugin.saveSettings();
-                }));
-
-        new Setting(containerEl)
-            .setName('Toggle code snippet')
-            .setDesc('Always show or hide Python code in the markdown preview.')
-            .addToggle(toggle => toggle
-                .setValue(this.plugin.settings.showCodeInPreview)
-                .onChange(async (value) => {
-                    this.plugin.settings.showCodeInPreview = value;
-                    await this.plugin.saveSettings();
-                }));
-
-        new Setting(containerEl)
-            .setName('Show exit code')
-            .setDesc('Toggle whether to show the exit code message after code execution.')
-            .addToggle(toggle => toggle
-                .setValue(this.plugin.settings.showExitCode)
-                .onChange(async (value) => {
-                    this.plugin.settings.showExitCode = value;
-                    await this.plugin.saveSettings();
-                }));
-    }
 }
